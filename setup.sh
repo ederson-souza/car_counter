41f6c98e6b03fdd4023965c64b43c59b3ea741c7
Éderson André de Souza <48734818+edinhoadm@users.noreply.github.com>
2021-02-07 00:00:41 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+sudo apt update
+sudo apt install gcc
+sudo apt install g++
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
