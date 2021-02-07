8362daed717705f5b1ef8fa451e7b183fff5a79b
Éderson André de Souza <48734818+edinhoadm@users.noreply.github.com>
2021-02-07 00:14:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ sudo apt update
 sudo apt install gcc
 sudo apt install g++
 pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+python -m pip install 'git+https://github.com/facebookresearch/detectron2.git
